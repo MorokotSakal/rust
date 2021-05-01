dcc3f2ca4b899b4279f8531a300c47ea48d0cf2d
sMorokot <sakalmorokott@gmail.com>
2021-05-01 08:34:33 +0700

    add datatypes files

---
+++ datatypes/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+
+    let x: i64 = 45; // i64 // normally it is i32
+
+    let y: u64 = 64; // unsigned
+
+    let f: f32 = 6.7; // f32
+
+    let b: bool = false;
+
+    println!("{}", x);
+
+    println!("{}", y);
+
+    println!("{}", f);
+
+    println!("{}", b);
+}
