c0d1f0d9de6f4f163ab79eff20fbb9bb585fcc00
sMorokot <sakalmorokott@gmail.com>
2021-05-01 08:35:21 +0700

    added if-statement and variables project

---
+++ if-statement/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    
+    let n = 30;
+
+    if n < 30 
+    {
+        println! ("The number is less than 30!");
+    }
+    else if n > 50
+    {
+        println! ("It is greater than 50!");
+    }
+    else
+    {
+        println!("It is 30!");
+    }
+}
---
+++ variables/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    
+    let mut x = 45; // mut is mutable which mean that it can be change
+
+    println!("The value of x is {}", x);
+
+    x = 60;
+
+    println!("The new value of x is {}", x);
+}
