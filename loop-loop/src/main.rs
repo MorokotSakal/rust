a1348b83c6992e6f9282c02dd303ca2e13281d16
sMorokot <sakalmorokott@gmail.com>
2021-05-01 08:33:37 +0700

    added loop loop project

---
+++ loop-loop/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    
+    let mut n = 0;
+
+    loop {
+        n += 1;
+
+        if n > 10000
+        {
+            break;
+        }
+
+        println! ("The value of n is {}", n);
+    }
+}
