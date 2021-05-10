55b0ca42cdbe21e6d29412d7a2d7f4429f90edb1
sMorokot <sakalmorokott@gmail.com>
2021-05-10 20:40:40 +0700

    added tuple structs code

---
+++ tuple-structs/src/main.rs
@@ -0,0 +1,14 @@
+
+// color: red, green, blue
+struct Color(u8, u8, u8);
+    
+fn main() {
+    
+    let mut red = Color(255, 0, 0);
+    
+    red.2 = 60;
+
+    println!("{}, {}, {}", red.0, red.1, red.2);
+    
+}
+    
